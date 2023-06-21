02792dc1c78c928fa9eb4e6434995da408b26639
JeromeC7 <jerome.cornet@sncf.fr>
2023-06-21 09:37:32 +0200

    modif test

---
+++ script.js
@@ -0,0 +1,7 @@
+let bouton = document.getElementById('boutonSubmit');
+alert("js ok");
+console.log('test js ok');
+bouton.addEventListener('click', function()  {
+    alert("Clické");
+    console.log('test');
+})
